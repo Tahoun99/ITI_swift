04eb6bddb0243977c63cad878e0460940a32c0d2
Ahmad Mohsen Tahoon <103882114+Tahoun99@users.noreply.github.com>
2024-10-07 05:18:44 +0300

    Add files via upload

---
+++ Meal Tracker/Meal Tracker/Food.swift
@@ -0,0 +1,13 @@
+//
+//  Food.swift
+//  Meal Tracker
+//
+//  Created by Ahmad Mohsen on 31/07/2024.
+//
+
+import Foundation
+
+struct Food {
+    var name: String
+    var description : String
+}
---
+++ Meal Tracker/Meal Tracker/Meal.swift
@@ -0,0 +1,13 @@
+//
+//  Meal.swift
+//  Meal Tracker
+//
+//  Created by Ahmad Mohsen on 31/07/2024.
+//
+
+import Foundation
+
+struct Meal {
+    var name : String
+    var food : [Food]
+}
